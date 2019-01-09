c06667de5ba137d4fed17175bb71ac7bcc633d37
kur <rhdnfka94@gmail.com>
2019-01-09 14:21:59 +0900

    Add instiz_core.all_coments_html

--- instiz_core.py
+++ instiz_core.py
@@ -1,10 +1,14 @@
 #-*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+
 import base
 from fp import cmap, pipe, cfilter
 
 INSTIZ_LIST_STEM = 'https://www.instiz.net/bbs/list.php'
 def article_stem(board_name):
     return 'https://www.instiz.net/%s' % str(board_name)
+INSTIZ_COMMENT_STEM = 'https://www.instiz.net/bbs/view_comment.php'
 
 def article_no_list(board_name, page_no, category):
     return base.article_no_list(
@@ -20,11 +24,33 @@ def article_no_list(board_name, page_no, category):
 
 def article_html_url(board_name, article_no):
     return base.article_html_url(
-        PPOM_ARTICLE_STEM, {'id':board_name, 'no':article_no}
+        article_stem(board_name), {'no':article_no}
     )
 
+def all_comments_html(board_name, article_no):
+    params = { 'id': board_name, 'no': str(article_no)}
+
+    resp = requests.post(INSTIZ_COMMENT_STEM, params=params)
+    resp.raise_for_status()
+    html = BeautifulSoup(resp.text, 'html.parser')
+    return html
+
+'''
 print(*article_no_list('fan', 1, 1), sep='\n')
 print( len(article_no_list('fan', 1, 1)) )
 
 print(article_stem('free'))
 
+html,url = article_html_url('name', 29866852)
+print(html, url)
+'''
+
+gen = \
+pipe(lambda html:html.find_all('div', class_='comment_line'),
+     cmap(lambda tag:tag.find('span')),
+     cfilter(lambda x:x is not None),
+     cmap(lambda span:span.text))
+
+html = all_comments_html('name', 29866852)
+for i,x in enumerate(gen(html)):
+    print('[%d]' % i, x)
