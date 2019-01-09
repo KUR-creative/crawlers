1c0472f438fd899d99fa74fd60a622e1a5df270d
kur <rhdnfka94@gmail.com>
2019-01-09 11:40:08 +0900

    Add ppom_core.article=>html,url

--- ppom_core.py
+++ ppom_core.py
@@ -3,6 +3,7 @@ import base
 from fp import cmap, pipe, cfilter
 
 PPOM_LIST_STEM = 'http://www.ppomppu.co.kr/zboard/zboard.php'
+PPOM_ARTICLE_STEM = 'http://www.ppomppu.co.kr/zboard/view.php'
 
 def article_no_list(board_name, page_no, category=''):
     html2no_list = \
@@ -23,10 +24,14 @@ def article_no_list(board_name, page_no, category=''):
         #lambda x:x
     )
 
+def article_html_url(board_name, article_no):
+    return base.article_html_url(
+        PPOM_ARTICLE_STEM, {'id':board_name, 'no':article_no}
+    )
+
 print(*article_no_list('humor', 1), sep='\n')
 print(len(article_no_list('humor', 1)))
-#print(article_no_list('humor', 1))
-#article_no_list('humor', 1)
-#print(article_no_list('humor', 5)[0])
 
+html,url = article_html_url('humor', 326422)
+print(html,url)
 
