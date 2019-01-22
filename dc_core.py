bffb4963a8d8dbef793564768075563969bd698f
kur <rhdnfka94@gmail.com>
2019-01-22 10:31:29 +0900

    Add unit test for 404 case

--- dc_core.py
+++ dc_core.py
@@ -20,9 +20,14 @@ def article_no_list(gall_id, page_no):
     )
 
 def article_html_url(gall_id, article_no):
-    return base.article_html_url(
-        DC_ARTICLE_STEM, {'id':gall_id, 'no':article_no}
-    )
+    try:
+        html,url = base.article_html_url(
+            DC_ARTICLE_STEM, {'id':gall_id, 'no':article_no}
+        )
+    except Exception as e:
+        html,url = 1,2
+    finally:
+        return html,url
 
 def comment_pages(article_html, article_url, gall_id, article_no):
     # comment page type: json
@@ -53,7 +58,7 @@ print(article_no_li, type(article_no_li[0]))
 
 html,url = article_html_url('programming', article_no_li[4])
 print(html, url)
-'''
+
 html,url = article_html_url('hit', 14933)
 cmt_dicts = comment_pages(html, url, 'hit', 14933)
 
@@ -62,3 +67,27 @@ cmt_dicts = comment_pages(html, url, 'programming', 969366)
 print(cmt_dicts)
 print(len(cmt_dicts))
 
+html,url = article_html_url('programming', 975801)
+cmt_dicts = comment_pages(html, url, 'programming', 975801)
+print(cmt_dicts)
+print(len(cmt_dicts))
+'''
+
+import vcr
+my_vcr = vcr.VCR(path_transformer=vcr.VCR.ensure_suffix('.yml'),
+                 cassette_library_dir='fixtures/cassettes')
+import unittest
+class test_article_html_url_core(unittest.TestCase):
+    @my_vcr.use_cassette
+    def test_If_404_then_Return_tuple_of_exception_url(self):
+        ret = article_html_url('programming',975801)
+        self.assertIsInstance(ret,tuple)
+        exception,url = ret
+        self.assertEqual(exception.status_code, 404)
+
+if __name__ == '__main__':
+    unittest.main()
+'''
+for no in range(975799,975804):
+    print( type(article_html_url('programming',no)) )
+'''
